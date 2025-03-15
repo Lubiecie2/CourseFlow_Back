1a2e81d2a38c16acfaa6e694538b1865c7354d6e
Kamil <kgruca7747@gmail.com>
2025-03-15 09:55:19 +0100

    Pierwszy commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+const cors = require("cors");
+const pool = require("./db");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use("/api", indexRouter);
+app.use("/api", usersRouter);
+
+module.exports = app;
---
+++ db.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DB_CONNECTION_STRING,
+});
+
+pool.on("connect", () => {
+  console.log("Connected to the PostgreSQL database.");
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
+
+module.exports = pool;
